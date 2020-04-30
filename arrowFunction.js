53121a78adcf46b8dd0fefc287f22317ed7aeb63
FirozKabir65 <firoj.kabir007@gmail.com>
2020-04-30 22:32:20 +0200

    An example about the importance of Arrow function

--- arrowFunction.js
+++ arrowFunction.js
@@ -33,5 +33,24 @@ document.addEventListener('click', () => console.log('click'))
 
 // An example
 class Person {
-    
+    constructor(name) {
+        this.name = name
+    }
+
+    printNameArrow() {
+        setTimeout(() => {
+            console.log('Arrow: ' + this.name)
+        }, 100)
+    }
+
+    printNameFunction() {
+        setTimeout(function() {
+            console.log('Function: ' + this.name)
+        }, 100)
+    }
 }
+
+let person = new Person('Rimon')
+person.printNameArrow()
+person.printNameFunction()
+console.log(this.name)
